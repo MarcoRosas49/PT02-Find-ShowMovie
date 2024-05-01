a76143674bf0a75e4615b71f94cf33295f1697b1
Marco <marcorosas1008@gmail.com>
2024-04-30 22:34:51 -0500

    Show&FindMovie

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { Movies } from './components/Movies.jsx'
+import { useMovies } from './hooks/useMovies.js'
+
+
+function App() {
+
+
+  const {movies} = useMovies()
+
+  return (
+    <div className='page'>
+
+      <header>
+        <h1>Buscador de Peliculas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, Star wars, the matrix ...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        <Movies movies={movies}/>
+      </main>
+
+
+    </div>
+  )
+}
+
+export default App
